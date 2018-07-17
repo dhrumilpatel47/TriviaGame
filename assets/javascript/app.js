3eaf0bb5835ddbc4ea2cb4c88d5bb1a255658a06
dhrumilpatel47 <dhrumilpatel47@gmail.com>
2018-07-16 20:08:53 -0400

    resubmit homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,151 +1,150 @@
-var triviaQuestions = [{
-	question: "How many NBA championships have the Lakers won?",
-	answerList: ["17", "16", "20", "14"],
-	answer: 1
-},{
-	question: "What city did the Lakers first start in?",
-	answerList: ["Los Angeles", "San Diego", "Sacramento", "Minneapolis"],
-	answer: 3
-},{
-	question: "Who was the tallest player in Lakers history?",
-	answerList: ["Will Chamberlain", "Shaquille o'neal", "Chuck Nevitt", "Kareem Abdul-Jabbar"],
-	answer: 2
-},{
-	question: "How many NBA Championships did Shaq and Kobe win together?",
-	answerList: ["4", "3", "5", "2"],
-	answer: 1
-},{
-	question: "How many NBA rings does Kobe Bryant have?",
-	answerList: ["6", "3", "1", "5"],
-	answer: 3
-},{
-	question: "Which Lakers player is on the NBA logo?",
-	answerList: ["Magic Johnson", "Kobe Bryant", "Elgin Baylor", "Jerry West"],
-	answer: 3
-},{
-	question: "Which NBA team are rivals with the Lakers?",
-	answerList: ["Boston Celtics", "Detroit Pistons", "San Antonio Spurs", "Sacamento Kings"],
-	answer: 0
-}];
-
-var picArray = ['1', '2', '3', '4', '5', '6', '7'];
-var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
-var messages = {
-	correct: "Yes, that's right!",
-	incorrect: "No, that's not it.",
-	endTime: "Out of time!",
-	finished: "Alright! Let's see how well you did."
-}
-
-$('#startBtn').on('click', function(){
-	$(this).hide();
-	newGame();
-});
-
-$('#startOverBtn').on('click', function(){
-	$(this).hide();
-	newGame();
-});
-
-function newGame(){
-	$('#finalMessage').empty();
-	$('#correctAnswers').empty();
-	$('#incorrectAnswers').empty();
-	$('#unanswered').empty();
-	currentQuestion = 0;
-	correctAnswer = 0;
-	incorrectAnswer = 0;
-	unanswered = 0;
-	newQuestion();
-}
-
-function newQuestion(){
-	$('#message').empty();
-	$('#correctedAnswer').empty();
-	$('#jpg').empty();
-	answered = true;
-	
-	// sets up new questions & answerList
-	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
-	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
-	for(var i = 0; i < 4; i++){
-		var choices = $('<div>');
-		choices.text(triviaQuestions[currentQuestion].answerList[i]);
-		choices.attr({'data-index': i });
-		choices.addClass('thisChoice');
-		$('.answerList').append(choices);
-	}
-
-	countdown();
-	//clicking an answer will pause the time and setup answerPage
-	$('.thisChoice').on('click',function(){
-		userSelect = $(this).data('index');
-		clearInterval(time);
-		answerPage();
-	});
-}
-
-function countdown(){
-	seconds = 15;
-	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
-	answered = true;
-	//sets timer to go down
-	time = setInterval(showCountdown, 1000);
-}
-
-function showCountdown(){
-	seconds--;
-	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
-	if(seconds < 1){
-		clearInterval(time);
-		answerPage();
-	}
-}
-
-function answerPage(){
-	$('#currentQuestion').empty();
-	$('.thisChoice').empty(); 
-	$('.question').empty();
-
-	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
-	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
-	$('#jpg').html('<img src = "assets/images/'+ picArray[currentQuestion] +'.jpg" width = "180px">');
-	//checks to see correct, incorrect, or unanswered
-	if((userSelect == rightAnswerIndex) && (answered == true)){
-		correctAnswer++;
-		$('#message').html(messages.correct);
-	} 	
-	else if((userSelect != rightAnswerIndex) && (answered == true)){
-		incorrectAnswer++;
-		$('#message').html(messages.incorrect);
-		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
-	} 	
-	else{
-		unanswered++;
-		$('#message').html(messages.endTime);
-		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
-		answered = true;
-	}	
-	if(currentQuestion == (triviaQuestions.length-1)){
-		setTimeout(scoreboard, 1000)
-	} 
-	else{
-		currentQuestion++;
-		setTimeout(newQuestion, 1000);
-	}	
-}
-
-function scoreboard(){
-	$('#timeLeft').empty();
-	$('#message').empty();
-	$('#correctedAnswer').empty();
-	$('#jpg').empty();
-
-	$('#finalMessage').html(messages.finished);
-	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
-	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
-	$('#unanswered').html("Unanswered: " + unanswered);
-	$('#startOverBtn').addClass('reset');
-	$('#startOverBtn').show();
-	$('#startOverBtn').html('Start Over?');
-}
+var questions = [{
+    question: 'How many NBA championships have the Lakers won?',
+    answers: ["Seventeen", "Sixteen", "Twenty", "Fourteen"],
+    correctAnswer: "Seventeen",
+    image:"assets/images/1.jpg"
+  }, {
+    question: 'What city did the Lakers first start in?',
+    answers: ["Los Angeles", "San Diego", "Sacramento", "Minneapolis"],
+    correctAnswer: "Minneapolis",
+    image:"assets/images/2.jpg"
+  }, {
+    question: 'Who was the tallest player in Lakers history?',
+    answers: ["Will Chamberlain", "Shaquille o'neal", "Chuck Nevitt", "Kareem Abdul-Jabbar"],
+    correctAnswer: "Chuck Nevitt",
+    image:"assets/images/3.jpg"
+  }, {
+    question: 'How many NBA Championships did Shaq and Kobe win together?',
+    answers: ["Four", "Three", "Five", "Two"],
+    correctAnswer: "Three",
+    image:"assets/images/4.jpg"
+  }, {
+    question: 'How many NBA rings does Kobe Bryant have?',
+    answers: ["Five", "Three", "One", "Five"],
+    correctAnswer: "Five",
+    image:"assets/images/5.jpg"
+  }, {
+    question: 'Which Lakers player is on the NBA logo?',
+    answers: ["Magic Johnson", "Kobe Bryant", "Elgin Baylor", "Jerry West"],
+    correctAnswer: "Jerry West",
+    image:"assets/images/6.jpg"
+  }, {
+    question: "Which NBA team are rivals with the Lakers?",
+    answers: ["Boston Celtics", "Detroit Pistons", "San Antonio Spurs", "Sacamento Kings"],
+    correctAnswer: "Boston Celtics",
+    image:"assets/images/7.jpg"
+  }];
+  
+  var panel = $('#quiz');
+  var countStartNumber = 15;
+  
+  
+  $(document).on('click', '#start-over', function(e) {
+    game.reset();
+  });
+  
+  $(document).on('click', '.answer-button', function(e) {
+    game.clicked(e);
+  });
+  
+  $(document).on('click', '#start', function(e) {
+    $('#subcontainer').prepend('<h3>Time Remaining: <span id="counter-number">15</span> Seconds</h3>');
+    game.loadQuestion();
+  });
+  
+  var game = {
+    questions:questions,
+    currentQuestion:0,
+    counter:countStartNumber,
+    correct:0,
+    incorrect:0,
+    countdown: function(){
+      game.counter--;
+      $('#counter-number').html(game.counter);
+      if (game.counter === 0){
+        console.log('TIME UP');
+        game.timeUp();
+      }
+    },
+  
+    loadQuestion: function(){
+      timer = setInterval(game.countdown, 1000);
+      panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+      for (var i = 0; i < questions[this.currentQuestion].answers.length; i++){
+        panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+      }
+    },
+    
+    nextQuestion: function(){
+      game.counter = countStartNumber;
+      $('#counter-number').html(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
+    },
+  
+    timeUp: function (){
+      clearInterval(timer);
+      $('#counter-number').html(game.counter);
+      panel.html('<h2>Out of Time!</h2>');
+      panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+      panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 2 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 2 * 1000);
+      }
+  
+    },
+    results: function() {
+      clearInterval(timer);
+      panel.html('<h2>All done, Lets see how you did!</h2>');
+      $('#counter-number').html(game.counter);
+      panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+      panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+      panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+      panel.append('<br><button id="start-over">Start Over?</button>');
+    },
+  
+    clicked: function(e) {
+      clearInterval(timer);
+      if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+        this.answeredCorrectly();
+      } else {
+        this.answeredIncorrectly();
+      }
+    },
+  
+    answeredIncorrectly: function() {
+      game.incorrect++;
+      clearInterval(timer);
+      panel.html('<h2>No, thats not right!</h2>');
+      panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 2 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 2 * 1000);
+      }
+    },
+  
+    answeredCorrectly: function(){
+      clearInterval(timer);
+      game.correct++;
+      panel.html('<h2>Yes, thats right!</h2>');
+      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 2 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 2 * 1000);
+      }
+    },
+  
+    reset: function(){
+      this.currentQuestion = 0;
+      this.counter = countStartNumber;
+      this.correct = 0;
+      this.incorrect = 0;
+      this.loadQuestion();
+    }
+  };
+  
